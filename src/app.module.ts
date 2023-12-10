4945984356731889fbc070486390a0c05441dc42
jocs <iqi.zamora@hotmail.com>
2023-12-10 00:21:07 -0600

    delete route product

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsController } from './products/products.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, ProductsController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
